6f8a49c7bd0a138a2a49020b9530d25e26fc5eb9
George Bolo <george.bolo@gmail.com>
2016-10-30 17:59:37 -0400

    chaincode invoke and query

--- script/bash/hyperledger.sh
+++ script/bash/hyperledger.sh
@@ -1,13 +1,20 @@
 # chaincode deploy
 /opt/fabric/peer/bin/peer --logging-level warning chaincode deploy \
--n cc2 -u jim \
+-u jim \
 -p github.com/hyperledger/fabric/examples/chaincode/go/chaincode_example02 \
--c '{"function":"init","args": ["a","100", "b", "200"]}'
+-c '{"function":"init","args": ["a","800", "b", "900"]}'
 
-/opt/fabric/peer/bin/peer --logging-level warning chaincode deploy \
--n cc4 -u jim \
--p github.com/hyperledger/fabric/examples/chaincode/go/chaincode_example04 \
--c '{"function":"init","args": ["some", "5"]}'
+# chaincode invoke
+/opt/fabric/peer/bin/peer --logging-level warning chaincode invoke \
+-n 8f566701539fa5b42cf521e91c8a5309a37dc59363c86a644214c28c85923fa81f7a02d03c015d22aef7b0bde8447f14a4f78dab4ab997f4bc06bb68f9377148 \
+-u jim \
+-c '{"args":["invoke", "a", "b", "100"]}'
+
+# chaincode query
+/opt/fabric/peer/bin/peer --logging-level warning chaincode query \
+-n 8f566701539fa5b42cf521e91c8a5309a37dc59363c86a644214c28c85923fa81f7a02d03c015d22aef7b0bde8447f14a4f78dab4ab997f4bc06bb68f9377148 \
+-u jim \
+-c '{"args":["query", "a"]}'
 
 # deploy membersrvc gui
 mkdir /tmp/db
